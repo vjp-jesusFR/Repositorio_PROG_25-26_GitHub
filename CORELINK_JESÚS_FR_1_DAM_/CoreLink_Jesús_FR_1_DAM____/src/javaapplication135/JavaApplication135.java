b83f27bf9d5be0566e60ff3c451bbc11ae683384
1DAM - JESUS Fernandez Ramos <jfernandezr24@informatica.iesvalledeljerteplasencia.es>
2026-04-17 17:55:58 +0200

    CORELINK

--- CORELINK_JESÚS_FR_1_DAM_/CoreLink_Jesús_FR_1_DAM____/src/javaapplication135/JavaApplication135.java
+++ CORELINK_JESÚS_FR_1_DAM_/CoreLink_Jesús_FR_1_DAM____/src/javaapplication135/JavaApplication135.java
@@ -45,47 +45,7 @@ public class JavaApplication135 {
             System.err.println("Error de lectura/escritura: " + e.getMessage());
         }
         
-        
-        //Fase 2
-      
-        System.err.println();
-        
-        // Nombres de archivos según el protocolo
-        String archivoEntrada2 = "04dedf8afeb8a4177a009f41bb78018af601e341ed4322d4073911c9ac879726.txt";
-        String archivoSalida2 = "claves.txt";
-        String criterio2 = "04217890";
-        
-//        int contadorSospechosos2 = 0;
-
        
-        try (BufferedReader br = new BufferedReader(new FileReader(archivoEntrada2));
-             PrintWriter pw = new PrintWriter(new FileWriter(archivoSalida2))) {
-
-            String linea;
-            
-            String cabecera = br.readLine();
-            if (cabecera != null) {
-                pw.println(cabecera);
-            }
-
-            
-            while ((linea = br.readLine()) != null) {
-                if (linea.contains(criterio2)) {
-                    pw.print(linea);
-                    
-                }
-            }
-
-            
-            
-            System.out.println("---------------------------------");
-
-        } catch (FileNotFoundException e) {
-            System.err.println("Error: No se encontró el archivo " + archivoEntrada);
-        } catch (IOException e) {
-            System.err.println("Error de lectura/escritura: " + e.getMessage());
-        }
-        
         
     }
 }
